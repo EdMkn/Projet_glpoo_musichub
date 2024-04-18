f90be51355c29660bc1f8e6141e21ecbda124a41
Manyim Ma Makon <nmanyim@rocketmail.com>
2024-04-18 22:07:17 +0200

    modif mineure

--- src/musichub/main/Main.java
+++ src/musichub/main/Main.java
@@ -1,4 +1,5 @@
 package musichub.main;
+
 import musichub.business.*;
 import musichub.util.*;
 import java.util.*;
@@ -6,97 +7,78 @@ import java.util.*;
 import java.beans.XMLEncoder;
 import java.io.FileOutputStream;
 import java.io.BufferedOutputStream;
-	
-public class Main
-{
- 	public static void main (String[] args) {
 
-		MusichubConsole console = new MusichubConsole ();
-		
+public class Main {
+	public static void main(String[] args) {
+
+		MusichubConsole console = new MusichubConsole();
+
 		System.out.println("Type h for available commands");
-		
-		
-		Scanner scan = new Scanner(System.in);
-		String choice = scan.nextLine();
-		
-		if (choice.length() == 0) System.exit(0);						
-		
-		while (choice.charAt(0)!= 'q') 	{
-			switch (choice.charAt(0)) 	{
-				case 'h':
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
-				case 't':
-					//album titles, ordered by date
+
+		Scanner scan = new Scanner(System.in).useDelimiter("\n");;
+		String choice = scan.next();
+
+		if (choice.length() == 0)
+			System.exit(0);
+
+		while (!choice.equals("q")) {
+			switch (choice) {
+				case "h":
+					break;
+				case "t":
+					// album titles, ordered by date
 					console.displayTAlbumByDate();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
-				case 'g':
-					//songs of an album, sorted by genre
+					break;
+				case "g":
+					// songs of an album, sorted by genre
 					console.displaySgAlbumByGenre();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
-				case 'd':
-					//songs of an album
+					break;
+				case "d":
+					// songs of an album
 					console.displaySgAlbum();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
-				case 'u':
-					//audiobooks ordered by author
+					break;
+				case "u":
+					// audiobooks ordered by author
 					console.displayAudiobookByAuthor();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
-				case 'c':
+					break;
+				case "c":
 					// add a new song
-          console.addSong();
-          console.printAvailableCommands();
-          choice = scan.nextLine();
-          break;
-				case 'a':
+					console.addSong();
+					break;
+				case "a":
 					// add a new album
 					console.addAlbum();
-          console.printAvailableCommands();
-          choice = scan.nextLine();
-				break;
-				case '+':
-					//add a song to an album:
-					console.addSgToAlbum();
-					console.printAvailableCommands();
-          choice = scan.nextLine();
 					break;
-				case 'l':
+				case "+":
+					// add a song to an album:
+					console.addSgToAlbum();
+					break;
+				case "l":
 					// add a new audiobook
 					console.addAudioBook();
-          console.printAvailableCommands();
-          choice = scan.nextLine();
-				break;
-				case 'p':
-					//create a new playlist from existing elements
-					console.createPlayList();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
 					break;
-				case '-':
-					//delete a playlist
+				case "p":
+					// create a new playlist from existing elements
+					console.createPlayList();
+					break;
+				case "-":
+					// delete a playlist
 					console.deletePlayList();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
-				case 's':
-					//save elements, albums, playlists
+					break;
+				case "s":
+					// save elements, albums, playlists
 					console.save();
-					console.printAvailableCommands();
-					choice = scan.nextLine();
-				break;
+					
+					break;
 				default:
-				
-				break;
+
+					break;
 			}
+			console.printAvailableCommands();
+			
+			while(!scan.hasNext())
+			scan.next();
+			choice = scan.nextLine();
 		}
 		scan.close();
 	}
